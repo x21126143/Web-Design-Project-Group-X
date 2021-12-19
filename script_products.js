5fe593af74656bf07a3d57ff18687dac0448f73b
x21130329 <x21130329@student.ncirl.ie>
2021-12-19 00:20:03 +0000

    Products with images

---
+++ script_products.js
@@ -0,0 +1,15 @@
+function bill1(){
+  alert("Your total bill is €49.99");
+}
+
+function bill2(){
+  alert("Your total bill is €69.99");
+}
+
+function bill3(){
+  alert("Your total bill is €14.99");
+}
+
+function bill4(){
+  alert("Your total bill is €29.99");
+}
