9b3c7aa29568bd3da4714853d0dcfb54ef248721
x21130329 <x21130329@student.ncirl.ie>
2021-12-19 17:33:19 +0000

    Products with quantity

--- script_products.js
+++ script_products.js
@@ -1,15 +1,43 @@
 function bill1(){
-  alert("Your total bill is €49.99");
+  var num = document.getElementById("quantity1").value;
+  var total = 0;
+  if(num > 0){
+    total = num * 49.99
+    alert("Your total bill is: " + '€' + total);
+  }else if(num <= 0){
+    alert("Enter a valid number");
+}
 }
 
 function bill2(){
-  alert("Your total bill is €69.99");
+  var num = document.getElementById("quantity2").value;
+  var total = 0;
+  if(num > 0){
+    total = num * 69.99
+    alert("Your total bill is: " + '€' + total);
+  }else if(num <= 0){
+    alert("Enter a valid number");
+}
 }
 
 function bill3(){
-  alert("Your total bill is €14.99");
+  var num = document.getElementById("quantity3").value;
+  var total = 0;
+  if(num > 0){
+    total = num * 14.99
+    alert("Your total bill is: " + '€' + total);
+  }else if(num <= 0){
+    alert("Enter a valid number");
+}
 }
 
 function bill4(){
-  alert("Your total bill is €29.99");
+  var num = document.getElementById("quantity4").value;
+  var total = 0;
+  if(num > 0){
+    total = num * 29.99
+    alert("Your total bill is: " + '€' + total);
+  }else if(num <= 0){
+    alert("Enter a valid number");
+}
 }
