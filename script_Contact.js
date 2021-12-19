c23d546802ffd1ff9330ea5534d91151c2f84efd
x21130329 <x21130329@student.ncirl.ie>
2021-12-19 20:24:54 +0000

    contact comments

--- script_Contact.js
+++ script_Contact.js
@@ -1,11 +1,12 @@
+//Function when contact form button clicked
 function submitForm(){
-  var verify = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  var firstName = document.getElementById("fName").value;
-  var lastName = document.getElementById("lName").value;
-  var email = document.getElementById("email").value;
-  if(firstName. length > 1 && lastName.length > 1 &&email.match(verify)){
-    alert("Your message has been submitted!");
+  var verify = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; //email format that will be matched off inputted email
+  var firstName = document.getElementById("fName").value; //Get first name info
+  var lastName = document.getElementById("lName").value; //Get last name info
+  var email = document.getElementById("email").value; //Get email name info
+  if(firstName. length > 1 && lastName.length > 1 &&email.match(verify)){ //If first and last name is filled out and the email is in correct format
+    alert("Your message has been submitted!");  //Successful form alert
   }else{
-    alert("Make sure your name email are correct");
+    alert("Make sure your name email are correct"); //If details incorrect have alert to tell user
   }
 }
